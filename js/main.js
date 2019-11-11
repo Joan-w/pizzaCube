41c500a285bbd4479ccf9bd336dba5f22fc441e4
murathe <isaacsm07p@gmail.com>
2019-11-11 08:29:09 +0300

    Create constructor

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function placeOrder(){
+    var pSize = document.getElementById("pizzaSize").value;
+    var pCrust = document.getElementById("pizzaCrust").value;
+    var pToppings = document.getElementById("pizzaToppings").value;
+
+    var grossPrice = 
+
+    function grossPrice (pSize, pCrust, pToppings){
+        this.pizzaSize = pSize;
+        this.pizzaCrust = pCrust;
+        this. pizzaToppings = pToppings;
+        this.location = delivery;
+    }
+
+
+
+
+    alert("Your order of a " + pSize + " pizza  with a crust of " + pCrust + " topped with " + pToppings + " has been received" );
+
+    prompt("Please enter your preffered delivery location.");
+
+    alert("Your order will be delivered at" + location + ", please have a total of" totalCost + " for payment.");
+
+
+
+
+}
