5c869924b02e65fdb09c11e63d574a5493582cfe
murathe <isaacsm07p@gmail.com>
2019-11-11 15:46:19 +0300

    Add totalCost func

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-function placeOrder(){
-    var pSize = document.getElementById("pizzaSize").value;
-    var pCrust = document.getElementById("pizzaCrust").value;
-    var pToppings = document.getElementById("pizzaToppings").value;
- 
+    var totalCost = [];
+    
+    
     function placeOrder (pSize, pCrust, pToppings){
         this.pizzaSize = pSize;
         this.pizzaCrust = pCrust;
@@ -10,6 +8,7 @@ function placeOrder(){
         this.location = delivery;
         this.price = 0;
     }
+    
     let sizes = ["large", "medium", "small"];
     let crust = ["chicago", "Neopoletan", "Deep-fish"];
     let toppings = ["pepperoni", "onions", "sausages"];
@@ -17,17 +16,22 @@ function placeOrder(){
 
     order.prototype.totalCost = function(){
         if (this.pizzaSize == sizes[0]) {
-            this.price += 2000;
+            alert("Your order for a " + pSize + " has been received!");
         } 
         else if(this.pizzaSize == sizes[1]){
             this.price += 1500;
         }
         else if(this.pizzaSize == sizes[2]){
             this.price += 1000;
-        }
-
-
+        }   
     }
 
 
+function placeOrder(){
+
+    var pSize = document.getElementById("pizzaSize").value;
+    var pCrust = document.getElementById("pizzaCrust").value;
+    var pToppings = document.getElementById("pizzaToppings").value;
+
+    alert("Your order of a " + this.pizzaSize + " has been received.");
 }
