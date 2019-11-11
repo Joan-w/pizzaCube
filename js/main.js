03dc73618f09ed069d61dac924dba60848b2c3dd
murathe <isaacsm07p@gmail.com>
2019-11-11 14:16:43 +0300

    Add constructor function

--- js/main.js
+++ js/main.js
@@ -2,24 +2,28 @@ function placeOrder(){
     var pSize = document.getElementById("pizzaSize").value;
     var pCrust = document.getElementById("pizzaCrust").value;
     var pToppings = document.getElementById("pizzaToppings").value;
-
-    var grossPrice = 
-
+ 
     function grossPrice (pSize, pCrust, pToppings){
         this.pizzaSize = pSize;
         this.pizzaCrust = pCrust;
         this. pizzaToppings = pToppings;
         this.location = delivery;
+        this.price
+    }
+    let sizes = ["large", "medium", "small"];
+    let crust = ["chicago", "Neopoletan", "Deep-fish"];
+    let toppings = ["pepperoni", "onions", "sausages"];
+
+
+
+    if (this.pizzaSize == sizes[0]) {
+        return 
+    } else if{
+        
+    } else if{
+        
     }
 
-
-
-
-    alert("Your order of a " + pSize + " pizza  with a crust of " + pCrust + " topped with " + pToppings + " has been received" );
-
-    prompt("Please enter your preffered delivery location.");
-
-    alert("Your order will be delivered at" + location + ", please have a total of" totalCost + " for payment.");
 
 
 
