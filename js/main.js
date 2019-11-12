7e923b53e29e09f1ca3bf350297b1905814c008a
murathe <isaacsm07p@gmail.com>
2019-11-12 11:53:29 +0300

    TotalCost function

--- js/main.js
+++ js/main.js
@@ -1,40 +1,24 @@
-    var totalCost = [];
-    
-    
-    function placeOrder (pSize, pCrust, pToppings){
-        this.pizzaSize = pSize;
-        this.pizzaCrust = pCrust;
-        this. pizzaToppings = pToppings;
-        this.location = delivery;
-        this.price = 0;
-    }
+    const order = document.getElementById("btn");
+    order.addEventListener("click", function(){
+        
+        var pSize = parseInt(document.getElementById("pizzaSize").value);
+        var pCrust = parseInt(document.getElementById("pizzaCrust").value);
+        var pToppings = parseInt(document.getElementById("pizzaToppings").value);
+        var quant = parseInt(document.getElementById("quantity").value);
+            
+        var totalCost = (pSize + pCrust + pToppings) * quant;
 
-    let sizes = ["large", "medium", "small"];
-    let crust = ["chicago", "Neopoletan", "Deep-fish"];
-    let toppings = ["pepperoni", "onions", "sausages"];
+        alert(`Your order has been received amounting to ${totalCost}`);
 
-
-    order.prototype.totalCost = function(){
-        if(this.pizzaSize === "" || this.pizzaCrust === "" || this.pizzaToppings === ""){
-            alert("Please specify your pizza choice!");
+        if (delivery.checked) {
+            prompt(`please enterb your location.`);
+        } else {
+            
         }
-        else if(this.pizzaSize == pizzaSize[0]) {
-            alert("Your order for a " + sizes + " has been received!");
-        } 
-        else if(this.pizzaSize == sizes[1]){
-            this.price += 1500;
-        }
-        else if(this.pizzaSize == sizes[2]){
-            this.price += 1000;
-        }   
-    }
+        prompt(``)
 
+        
 
-function placeOrder(){
+    
+    });
 
-    var pSize = document.getElementById("pizzaSize").value;
-    var pCrust = document.getElementById("pizzaCrust").value;
-    var pToppings = document.getElementById("pizzaToppings").value;
-
-    alert("Your order of a " + this.pizzaSize + " has been received.");
-}
